c652df90ca1d27f4fad87b215936187b2f21ed29
Yiğit Can TEKE <yigitcanteke@Oremas-iMac.local>
2023-05-16 11:05:51 +0300

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -6,18 +6,16 @@ import { TodoProvider } from './context/TodoContext';
 import './App.css';
 
 export default function App() {
-  export default function App() {
-    return (
-      <TodoProvider>
-        <div className="App">
-          <div className='container'>
-            <h1>Todo App</h1>
-            <TodoAdd />
-            <TodoList />
-            <Footer />
-          </div>
+  return (
+    <TodoProvider>
+      <div className="App">
+        <div className='container'>
+          <h1>Todo App</h1>
+          <TodoAdd />
+          <TodoList />
+          <Footer />
         </div>
-      </TodoProvider>
-    </TodoProvider >
+      </div>
+    </TodoProvider>
   );
-  }
+}
